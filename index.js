f3571216cb14b7557eae70f217f1a9ba9c7f86c9
TMania808 <83692753+TMania808@users.noreply.github.com>
2022-10-24 21:49:43 -0700

    refactored symbol/number conditional checks into one function

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ let lightBtn = document.getElementById("btnLightMode")
 let numberCheck = document.getElementById("numberCheckBox")
 let symbolCheck = document.getElementById("symbolCheckBox")
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const numbersArray = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+]
+
+const symbolsArray = [
+    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+]
 
 password1.addEventListener("click", function() {
     copyPassword("password1")
@@ -20,110 +27,37 @@ password2.addEventListener("click", function() {
 })
 
 function toggleNumbers() {
-    if (numberCheck.checked === true) {
-        numbers = true
-    } else {
-        numbers = false
-    }
+    numbers = numberCheck.checked
 }
 
 function toggleSymbols() {
-    if (symbolCheck.checked === true) {
-        symbols = true
-    } else {
-        symbols = false
-    }
+    symbols = symbolCheck.checked
 }
 
 function generatePassword() {
     password1.textContent = ""
     password2.textContent = ""
     
-    if (numbers === true && symbols === true) {
-        for (let i = 0; i < passwordLength.value; i++) {
-            let randomIndex = Math.floor(Math.random() * characters.length)
-            password1.textContent += characters[randomIndex]
-        }
-    
-        for (let i = 0; i < passwordLength.value; i++) {
-            let randomIndex = Math.floor(Math.random() * characters.length)
-            password2.textContent += characters[randomIndex]
-        }
-    } else if (numbers === true && symbols === false) {
-            for (let i = 0; i < passwordLength.value; i++) {
-                let randomIndex = Math.floor(Math.random() * 62)
-                password1.textContent += characters[randomIndex]
-            }
-    
-            for (let i = 0; i < passwordLength.value; i++) {
-                let randomIndex = Math.floor(Math.random() * 62)
-                password2.textContent += characters[randomIndex]
-            }
-    } else if (numbers === false && symbols === false) {
-            for (let i = 0; i < passwordLength.value; i++) {
-                let randomIndex = Math.floor(Math.random() * 51)
-                password1.textContent += characters[randomIndex]
-        }
-    
-            for (let i = 0; i < passwordLength.value; i++) {
-                let randomIndex = Math.floor(Math.random() * 51)
-                password2.textContent += characters[randomIndex]
-        }
-    } else if (numbers === false && symbols === true) {
-        for (let i = 0; i < passwordLength.value; i++) {
-            let randomIndex = Math.floor(Math.random() * characters.length)
-            if (characters[randomIndex] === "0") {
-                i--
-            } else if (characters[randomIndex] === "1") {
-                i--
-            } else if (characters[randomIndex] === "2") {
-                i--
-            } else if (characters[randomIndex] === "3") {
-                i--
-            } else if (characters[randomIndex] === "4") {
-                i--
-            } else if (characters[randomIndex] === "5") {
-                i--
-            } else if (characters[randomIndex] === "6") {
-                i--
-            } else if (characters[randomIndex] === "7") {
-                i--
-            } else if (characters[randomIndex] === "8") {
-                i--
-            } else if (characters[randomIndex] === "9") {
-                i--
-            } else {
-                password1.textContent += characters[randomIndex]
-            }       
-        }
-    
-        for (let i = 0; i < passwordLength.value; i++) {
-            let randomIndex = Math.floor(Math.random() * characters.length)
-            if (characters[randomIndex] === "0") {
-                i--
-            } else if (characters[randomIndex] === "1") {
-                i--
-            } else if (characters[randomIndex] === "2") {
-                i--
-            } else if (characters[randomIndex] === "3") {
-                i--
-            } else if (characters[randomIndex] === "4") {
-                i--
-            } else if (characters[randomIndex] === "5") {
-                i--
-            } else if (characters[randomIndex] === "6") {
-                i--
-            } else if (characters[randomIndex] === "7") {
-                i--
-            } else if (characters[randomIndex] === "8") {
-                i--
-            } else if (characters[randomIndex] === "9") {
-                i--
-            } else {
-                password2.textContent += characters[randomIndex]
-            }       
-        }
+    let array = []
+
+    if (numbers && !symbols) {
+        array = characters.concat(numbersArray)
+    } else if (!numbers && symbols) {
+        array = characters.concat(symbolsArray)
+    } else if (numbers && symbols) {
+        array = characters.concat(numbersArray).concat(symbolsArray)
+    } else {
+        array = characters
     }
+
+    for (let i = 0; i < passwordLength.value; i++) {
+        let randomIndex01 = Math.floor(Math.random() * array.length)
+        let randomIndex02 = Math.floor(Math.random() * array.length)
+
+        password1.textContent += array[randomIndex01]
+        password2.textContent += array[randomIndex02]
+    }
+
 }
 
 function copyPassword(string) {
